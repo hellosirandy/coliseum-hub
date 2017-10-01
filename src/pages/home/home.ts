87ceb0bb466bb02c00d1fcd59aee91592a0837d6
hellosirandy <sac840711@gmail.com>
2017-10-01 23:59:08 +0800

    working on ion slide

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,26 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController, NavController } from 'ionic-angular';
 import { LeagueViewPage } from '../league-view/league-view';
 import { EditStadiumPage } from '../edit-stadium/edit-stadium';
 
 import { AuthService } from '../../providers/auth.service';
+import { DatabaseService } from '../../providers/database.service';
 
 import { Leagues } from '../../statics/sports-leagues-teams'
+import { Stadium } from '../../models/stadium'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
   user=null;
-  stadiums=[];
+  mlbStadiums:Stadium[]=[];
 
+  category: any={}
+  
   constructor(
     private auth: AuthService,
+    private database: DatabaseService,
     private modalCtrl: ModalController,
     private navCtrl: NavController,
   ) {
 
+  }
+
+  ngOnInit() {
+    this.database.getLeagueStadium('MLB').subscribe((res: Stadium[]) => {
+      this.mlbStadiums = res.slice(0, 6)
+    })
   }
 
   ionViewDidLoad() {
